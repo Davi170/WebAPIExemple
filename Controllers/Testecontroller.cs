09462ed3124c034ed8e39cedd55072d6241617f3
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-08-10 16:52:27 -0300

    implementador de teste

--- Controllers/Testecontroller.cs
+++ Controllers/Testecontroller.cs
@@ -1,2 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 
+[ApiController]
+[Route("test/")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "My app is Working!";
+    }
+}
