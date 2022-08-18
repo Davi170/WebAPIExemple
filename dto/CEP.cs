e847d8c6b7f4136ac9452f18cd2d0434ff7caf52
Pedrohhenrique <pedrohdosantosss@gmail.com>
2022-08-18 15:12:52 -0300

    novo arquivo do CEP

---
+++ dto/CEP.cs
@@ -0,0 +1,8 @@
+public class CEP
+{
+    public string Logradouro {get; set;}
+    public string Complemento {get; set;}
+    public string Cep {get; set;}
+    public string Bairro {get; set;}
+    public string uf {get; set;}
+}
